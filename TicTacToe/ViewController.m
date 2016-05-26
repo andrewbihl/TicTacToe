b3a6140b6ffd2bfdeb552dceaae2f52782273825
andrewbihl <andrewbihlva@gmail.com>
2016-05-26 15:34:20 -0500

    Enter logic for checking whether someone has won

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+//Board values. 0=none, 1=first player, 2=second player
+@property NSMutableArray* boardValues;
 @property (weak, nonatomic) IBOutlet UIButton *button1;
 @property (weak, nonatomic) IBOutlet UIButton *button2;
 @property (weak, nonatomic) IBOutlet UIButton *button3;
@@ -20,6 +22,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *button9;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property bool firstPlayersTurn;
+@property NSArray* winningSets;
 
 @end
 
@@ -27,23 +30,71 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.boardValues = [NSMutableArray arrayWithObjects:@0,@0,@0,@0,@0,@0,@0,@0,@0,nil];
+    self.winningSets = [self getSetOfWinningSets];
+    self.firstPlayersTurn = true;
     self.whichPlayerLabel.text = @"x";
+    self.whichPlayerLabel.textColor = [UIColor blueColor];
+    NSLog(@"%@",self.boardValues);
 }
 
-- (IBAction)onButtonPushed:(UIButton *)sender {
-    self.firstPlayersTurn = !self.firstPlayersTurn;
-    if (self.firstPlayersTurn) {
-        self.whichPlayerLabel.text = @"x";
-    } else {
-        self.whichPlayerLabel.text = @"o";
-    }
-    if (self.firstPlayersTurn == YES) {
-        [sender setTitle:@"x" forState:UIControlStateNormal];
-    } else {
-        [sender setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
-        [sender setTitle:@"o" forState:UIControlStateNormal];
+-(NSArray*)getSetOfWinningSets{
+    NSMutableArray* result = [NSMutableArray new];
+    [result addObject: [NSSet setWithObjects:@0,@1,@2, nil]];
+    [result addObject: [NSSet setWithObjects:@3,@4,@5, nil]];
+    [result addObject: [NSSet setWithObjects:@6,@7,@8, nil]];
+    [result addObject: [NSSet setWithObjects:@0,@4,@8, nil]];
+    [result addObject: [NSSet setWithObjects:@2,@4,@6, nil]];
+    [result addObject: [NSSet setWithObjects:@0,@3,@6, nil]];
+    [result addObject: [NSSet setWithObjects:@1,@4,@7, nil]];
+    [result addObject: [NSSet setWithObjects:@2,@5,@8, nil]];
+    
+    return result;
+}
+
+-(NSString*)whoWon{
+    
+    NSMutableSet* xSpots = [NSMutableSet new];
+    NSMutableSet* oSpots = [NSMutableSet new];
+    
+    for (int i = 0; i < self.boardValues.count; i++){
+        NSNumber* x = [self.boardValues objectAtIndex:i];
+        if ( x.integerValue>0){
+            if (x.integerValue==1)
+                [xSpots addObject:[NSNumber numberWithInteger:i]];
+            else
+                [oSpots addObject:[NSNumber numberWithInteger:i]];
+        }
     }
     
+    for (NSSet* set in self.winningSets){
+        if ([set isSubsetOfSet:xSpots])
+            return @"PLAYER 1";
+        else if ([set isSubsetOfSet:oSpots])
+            return @"PLAYER 2";
+    }
+    return @"No";
+}
+
+
+- (IBAction)onButtonPushed:(UIButton *)sender {
+    int boardIndex = [sender.restorationIdentifier integerValue];
+    if (self.firstPlayersTurn) {
+        [self.boardValues replaceObjectAtIndex:boardIndex withObject:@1];
+        [sender setTitle:@"x" forState:UIControlStateNormal];
+        self.whichPlayerLabel.text = @"o";
+        self.whichPlayerLabel.textColor = [UIColor redColor];
+    }
+    else {
+        [self.boardValues replaceObjectAtIndex:boardIndex withObject:@2];
+        [sender setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        [sender setTitle:@"o" forState:UIControlStateNormal];
+        self.whichPlayerLabel.text = @"x";
+        self.whichPlayerLabel.textColor = [UIColor blueColor];
+    }
+    NSLog(@"%@",self.boardValues);
+    NSLog(@"%@",[self whoWon]);
+    self.firstPlayersTurn = !self.firstPlayersTurn;
 }
 
 @end
